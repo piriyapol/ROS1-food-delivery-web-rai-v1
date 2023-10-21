645bfe1219c0f9f05b15e96fc6a5a5287067efa8
Piriyapol Prasankliew <29841795+piriyapol@users.noreply.github.com>
2023-10-21 16:35:11 +0700

    fixed order controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,32 +1,75 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+// Separate function to find table information
+const findTableInformation = async (tableNumber, prisma) => {
+  return prisma.tableInformation.findUnique({
+    where: {
+      table_number: tableNumber,
+    },
+  });
+};
+
+
+// Log the table_id when createOrder is called
+const logTableId = async (tableNumber) => {
+  try {
+    // Convert the tableNumber to a string (if it's not already)
+    const tableNumberStr = tableNumber.toString();
+
+    // Debug: Print the tableNumber
+    console.log("Table Number:", tableNumberStr);
+
+    // Log the where clause
+    console.log("Where Clause:", {
+      table_number: tableNumberStr,
+    });
+
+    const tableInfo = await findTableInformation(tableNumberStr, prisma);
+
+    // Debug: Print the table_id if found
+    if (tableInfo) {
+      console.log("Table ID:", tableInfo.table_id);
+    } else {
+      console.log("Table not found");
+    }
+  } catch (error) {
+    console.error("Error logging table_id:", error);
+  }
+};
+
 // Create a new order
 const createOrder = async (req, res) => {
-  const {
-    table_id,
-    status,
-    total_price,
-    special_requests,
-    customer_name,
-    order_items,
-  } = req.body;
+  const { tableNumber, orderData } = req.body;
 
   try {
+    const tableNumberStr = tableNumber.toString();
+    console.log("Table Number:", tableNumberStr);
+    logTableId(tableNumberStr, prisma);
+
+    const tableInfo = await findTableInformation(tableNumberStr, prisma);
+    console.log("Table Information:", tableInfo);
+
+    if (!tableInfo) {
+      return res.status(404).json({ error: "Table not found" });
+    }
+
     const order = await prisma.order.create({
       data: {
-        table_id,
-        status,
-        total_price,
-        special_requests,
-        customer_name,
+        table_id: tableInfo.table_id,
+        status: orderData.status,
+        total_price: orderData.total_price,
+        special_requests: orderData.special_requests,
+        customer_name: orderData.customer_name,
         OrderItem: {
-          create: order_items,
+          // Use "OrderItem" instead of "order_items"
+          create: orderData.order_items,
         },
       },
       include: {
         tableInformation: true,
         OrderItem: {
+          // Use "OrderItem" instead of "order_items"
           include: {
             menu_item: true,
           },
@@ -36,10 +79,11 @@ const createOrder = async (req, res) => {
 
     res.status(201).json(order);
   } catch (error) {
-    console.error("Error creating order:", error); // Log the error
+    console.error("Error creating order:", error);
     res.status(500).json({ error: "Internal server error (Create Order)" });
   }
 };
+
 
 // Retrieve a list of orders
 const getOrders = async (req, res) => {
@@ -75,7 +119,7 @@ const serveOrder = async (req, res) => {
       },
       include: {
         tableInformation: true,
-        order_items: {
+        OrderItem: {
           include: {
             menu_item: true,
           },
