7df88858cc1f126a95635e30bc811a0211e76fb0
Piriyapol Prasankliew <29841795+piriyapol@users.noreply.github.com>
2023-10-21 18:09:41 +0700

    Fixed customer interfaace function.

--- frontend/src/components/CustomerInterface.js
+++ frontend/src/components/CustomerInterface.js
@@ -7,8 +7,9 @@ import {
   Row,
   Col,
   ListGroup,
+  Modal,
 } from "react-bootstrap";
-import { getMenuItems, placeOrder } from "../services/api"; // Import the API functions
+import { getMenuItems, placeOrder } from "../services/api";
 
 function CustomerInterface() {
   const [menuItems, setMenuItems] = useState([]);
@@ -17,6 +18,24 @@ function CustomerInterface() {
   const [customerName, setCustomerName] = useState("");
   const [selectedMenuItems, setSelectedMenuItems] = useState([]);
   const [tableNumber, setTableNumber] = useState(1);
+  const [specialRequests, setSpecialRequests] = useState("");
+  const [orderPlaced, setOrderPlaced] = useState(false);
+  const [orderError, setOrderError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleQuantityChange = (itemId, newQuantity) => {
+    const updatedItems = selectedMenuItems.map((item) => {
+      if (item.item_id === itemId) {
+        const parsedQuantity = parseInt(newQuantity, 10);
+        if (!isNaN(parsedQuantity) && parsedQuantity >= 0) {
+          item.quantity = parsedQuantity;
+        }
+      }
+      return item;
+    });
+
+    setSelectedMenuItems(updatedItems);
+  };
 
   useEffect(() => {
     async function fetchMenu() {
@@ -41,58 +60,131 @@ function CustomerInterface() {
 
   const handleMenuSelection = (itemId) => {
     const selectedMenuItem = menuItems.find((item) => item.item_id === itemId);
-    setSelectedMenuItems((prevItems) => [...prevItems, selectedMenuItem]);
+    const existingItem = selectedMenuItems.find(
+      (item) => item.item_id === itemId,
+    );
+
+    if (existingItem) {
+      existingItem.quantity += 1;
+      setSelectedMenuItems([...selectedMenuItems]);
+    } else {
+      setSelectedMenuItems([
+        ...selectedMenuItems,
+        { ...selectedMenuItem, quantity: 1 },
+      ]);
+    }
+  };
+
+  const handleIncreaseQuantity = (itemId) => {
+    const updatedItems = selectedMenuItems.map((item) => {
+      if (item.item_id === itemId) {
+        item.quantity += 1;
+      }
+      return item;
+    });
+
+    setSelectedMenuItems(updatedItems);
+  };
+
+  const handleDecreaseQuantity = (itemId) => {
+    const updatedItems = selectedMenuItems.map((item) => {
+      if (item.item_id === itemId) {
+        if (item.quantity > 1) {
+          item.quantity -= 1;
+        }
+      }
+      return item;
+    });
+
+    setSelectedMenuItems(updatedItems);
+  };
+
+  const handleRemoveMenuItem = (itemId) => {
+    const updatedItems = selectedMenuItems.filter(
+      (item) => item.item_id !== itemId,
+    );
+    setSelectedMenuItems(updatedItems);
   };
 
   const handlePlaceOrder = async () => {
     const orderData = {
       table_id: tableNumber,
       status: "pending",
-      total_price: 0,
-      special_requests: "No special requests",
-      customer_name: customerName,
-      order_items: selectedMenuItems.map((item) => ({
+      total_price: totalOrderPrice,
+      specialRequests: specialRequests,
+      customerName: customerName,
+      orderItems: selectedMenuItems.map((item) => ({
         item_id: item.item_id,
-        quantity: 1, // You can allow the customer to choose quantity
+        quantity: item.quantity,
       })),
     };
 
     try {
       const response = await placeOrder(tableNumber, orderData);
       console.log("Order placed successfully:", response);
-      // You can add further handling here, such as showing a success message.
+      setOrderPlaced(true);
+      setOrderError(null);
+      setShowModal(true);
     } catch (error) {
       console.error("Error placing the order:", error);
-      setError("Failed to place the order.");
+      setOrderPlaced(false);
+      setOrderError(error.response?.data?.error || "Failed to place the order");
+      setShowModal(true);
     }
   };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const totalOrderPrice = selectedMenuItems.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0,
+  );
 
   return (
     <Container>
       <Row>
-        <Col>
-          <Card>
-            <Card.Body>
-              <h2>Customer Interface</h2>
-              <Form>
-                <Form.Group controlId="tableNumber">
-                  <Form.Label>Table Number</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter table number"
-                    value={tableNumber}
-                    onChange={(e) => setTableNumber(e.target.value)}
-                  />
-                </Form.Group>
-                <Form.Group controlId="customerName">
-                  <Form.Label>Customer Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter customer name"
-                    value={customerName}
-                    onChange={(e) => setCustomerName(e.target.value)}
-                  />
-                </Form.Group>
+        <Col lg={6} md={12}>
+          {orderPlaced ? (
+            <div>
+              <h2>Order Already Placed</h2>
+              <p>You have already placed an order. Thank you!</p>
+            </div>
+          ) : (
+            <Card>
+              <Card.Body>
+                <h2>Customer Interface</h2>
+                <Form>
+                  <Form.Group controlId="tableNumber">
+                    <Form.Label>Table Number</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter table number"
+                      value={tableNumber}
+                      onChange={(e) => setTableNumber(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="customerName">
+                    <Form.Label>Customer Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter customer name"
+                      value={customerName}
+                      onChange={(e) => setCustomerName(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="specialRequests">
+                    <Form.Label>Special Requests</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows={3}
+                      placeholder="Enter special requests"
+                      value={specialRequests}
+                      onChange={(e) => setSpecialRequests(e.target.value)}
+                    />
+                  </Form.Group>
+                </Form>
                 <Button
                   variant="primary"
                   type="button"
@@ -100,11 +192,11 @@ function CustomerInterface() {
                 >
                   Submit Order
                 </Button>
-              </Form>
-            </Card.Body>
-          </Card>
+              </Card.Body>
+            </Card>
+          )}
         </Col>
-        <Col>
+        <Col lg={3} md={12}>
           <Card>
             <Card.Body>
               <h2>Menu</h2>
@@ -129,21 +221,73 @@ function CustomerInterface() {
             </Card.Body>
           </Card>
         </Col>
-        <Col>
+        <Col lg={3} md={12}>
           <Card>
             <Card.Body>
               <h2>Selected Items</h2>
               <ListGroup>
                 {selectedMenuItems.map((item) => (
                   <ListGroup.Item key={item.item_id}>
-                    {item.item_name} - ${item.price}
+                    {item.item_name} - ${item.price} - Quantity:
+                    <input
+                      type="number"
+                      value={item.quantity}
+                      onChange={(e) =>
+                        handleQuantityChange(item.item_id, e.target.value)
+                      }
+                    />
+                    <Button
+                      variant="success"
+                      size="sm"
+                      onClick={() => handleIncreaseQuantity(item.item_id)}
+                    >
+                      +
+                    </Button>
+                    <Button
+                      variant="warning"
+                      size="sm"
+                      onClick={() => handleDecreaseQuantity(item.item_id)}
+                    >
+                      -
+                    </Button>
+                    <Button
+                      variant="danger"
+                      size="sm"
+                      onClick={() => handleRemoveMenuItem(item.item_id)}
+                    >
+                      Remove
+                    </Button>
                   </ListGroup.Item>
                 ))}
+                {selectedMenuItems.length > 0 && (
+                  <ListGroup.Item>
+                    Total Price: ${totalOrderPrice.toFixed(2)}
+                  </ListGroup.Item>
+                )}
               </ListGroup>
             </Card.Body>
           </Card>
         </Col>
       </Row>
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {orderPlaced ? "Order Placed" : "Order Failed"}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {orderPlaced ? (
+            <p>Your order has been placed successfully. Thank you!</p>
+          ) : (
+            <p>{orderError}</p>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleCloseModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 }
